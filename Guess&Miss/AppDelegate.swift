a8863cb55b8e182539c410a4df8025b3e7972fb9
Demistry <dave11ilen@gmail.com>
2019-05-13 16:14:43 +0100

    Initial Commit

---
+++ Guess&Miss/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Guess&Miss
+//
+//  Created by David Ilenwabor on 13/05/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Guess&Miss/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Guess&Miss
+//
+//  Created by David Ilenwabor on 13/05/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
