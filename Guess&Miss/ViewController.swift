bbd0134d99a4dd1801bfdf3b34db6c5f0a66d917
Demistry <dave11ilen@gmail.com>
2019-05-22 17:14:16 +0100

    Added logic for comparing guesses and answer

--- Guess&Miss/ViewController.swift
+++ Guess&Miss/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: NSViewController {
     fileprivate var answer : String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startGame()
         // Do any additional setup after loading the view.
     }
 
@@ -33,14 +33,27 @@ class ViewController: NSViewController {
     @IBAction func makeGuess(_ sender: NSButton) {
     
         let guess = textField.stringValue
-        textField.stringValue = ""
+        guard Set(guess).count == 4 else{
+            return
+        }
+        
+        let badChars = CharacterSet(charactersIn : "0123456789").inverted
+        guard guess.rangeOfCharacter(from: badChars) == nil else{
+            return
+        }
         guesses.insert(guess, at: 0)
-        tableView.reloadData()
+        tableView.insertRows(at: IndexSet(integer: 0), withAnimation: .slideDown)
+        
+//        if result(for: guess).contains("4b"){
+//            let alert = NSAlert
+//        }
     }
     
     fileprivate func startGame(){
+        textField.stringValue = ""
         guesses.removeAll()
         var numbers = Array(0...9)
+        answer = ""
         numbers = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: numbers) as! [Int]
         
         for _ in 0..<4{
@@ -48,6 +61,23 @@ class ViewController: NSViewController {
         }
         
         tableView.reloadData()
+    }
+    
+    fileprivate func result(for guess : String)->String{
+        var bulls = 0
+        var cows = 0
+        
+        let guessLetters = Array(guess)
+        let answerLetters = Array(answer)
+        
+        for (index, letter) in guessLetters.enumerated(){
+            if letter == answerLetters[index]{
+                bulls += 1
+            } else if answerLetters.contains(letter){
+                cows += 1
+            }
+        }
+        return "\(bulls)b \(cows)c"
     }
 }
 
@@ -63,7 +93,7 @@ extension ViewController : NSTableViewDelegate, NSTableViewDataSource{
         if tableColumn?.title == "Guesses"{
             view.textField?.stringValue = guesses[row]
         } else{
-            
+            view.textField?.stringValue = result(for: guesses[row])
         }
         
         return view
